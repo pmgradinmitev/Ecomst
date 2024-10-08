cbaacf074230060cae65ad412d7c009e4f598997
pmg.tadinmitev92 <pmg.tadinmitev92@gmail.com>
2024-10-08 10:50:52 +0300

    Commented upsert from category controller. Added create and update action. Added create and update view.

--- Ecomst/Controllers/CategoryController.cs
+++ Ecomst/Controllers/CategoryController.cs
@@ -19,30 +19,35 @@ namespace Ecomst.Controllers
             return View(categoryList);
         }
 
-        public IActionResult Upsert(int? id)
+        //public IActionResult Upsert(int? id)
+        //{
+        //    CategoryViewModel viewModel = new CategoryViewModel();
+        //    if(id == null)
+        //        return View(viewModel);
+
+        //    Category? category = _categoryService.GetCategoryById(id);
+        //    if (category == null)
+        //    {
+        //        TempData["error"] = "Category with id " + id + " not found!";
+        //        return RedirectToAction("Index");
+        //    }
+        //    viewModel.PopulateFromCategory(category);
+        //    return View(viewModel);
+        //}
+
+        public IActionResult Create()
         {
             CategoryViewModel viewModel = new CategoryViewModel();
-            if(id == null)
-                return View(viewModel);
-
-            Category? category = _categoryService.GetCategoryById(id);
-            if (category == null)
-            {
-                TempData["error"] = "Category with id " + id + " not found!";
-                return RedirectToAction("Index");
-            }
-            viewModel.PopulateFromCategory(category);
             return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(CategoryViewModel viewModel)
+        public IActionResult Create(CategoryViewModel viewModel)
         {
-            Category category = new Category();
-            
-            if (viewModel.Id == null && ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                Category category = new Category();
                 viewModel.PopulateCategory(category);
                 if (_categoryService.AddCategory(category))
                 {
@@ -51,21 +56,71 @@ namespace Ecomst.Controllers
                 }
                 TempData["error"] = "Unable to create category!";
             }
-            else
+            return View(viewModel);
+        }
+
+        public IActionResult Update(int id)
+        {
+            Category? category = _categoryService.GetCategoryById(id);
+            if (category == null)
             {
-                if (ModelState.IsValid)
+                TempData["error"] = "Category with id " + id + " not found!";
+                return RedirectToAction("Index");
+            }
+            CategoryViewModel viewModel = new CategoryViewModel();
+            viewModel.PopulateFromCategory(category);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(CategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Category category = new Category();
+                viewModel.PopulateCategory(category);
+                if (_categoryService.UpdateCategory(category))
                 {
-                    viewModel.PopulateCategory(category);
-                    if (_categoryService.UpdateCategory(category))
-                    {
-                        TempData["success"] = "Category was updated successfully!";
-                        return RedirectToAction("Index");
-                    }
-                    TempData["error"] = "Unable to update category!";
+                    TempData["success"] = "Category was updated successfully!";
+                    return RedirectToAction("Index");
                 }
+                TempData["error"] = "Unable to update category!";
             }
             return View(viewModel);
         }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(CategoryViewModel viewModel)
+        //{
+        //    Category category = new Category();
+            
+        //    if (viewModel.Id == null && ModelState.IsValid)
+        //    {
+        //        viewModel.PopulateCategory(category);
+        //        if (_categoryService.AddCategory(category))
+        //        {
+        //            TempData["success"] = "Category was created successfully!";
+        //            return RedirectToAction("Index");
+        //        }
+        //        TempData["error"] = "Unable to create category!";
+        //    }
+        //    else
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            viewModel.PopulateCategory(category);
+        //            if (_categoryService.UpdateCategory(category))
+        //            {
+        //                TempData["success"] = "Category was updated successfully!";
+        //                return RedirectToAction("Index");
+        //            }
+        //            TempData["error"] = "Unable to update category!";
+        //        }
+        //    }
+        //    return View(viewModel);
+        //}
 
         [HttpPost]
         [ValidateAntiForgeryToken]
