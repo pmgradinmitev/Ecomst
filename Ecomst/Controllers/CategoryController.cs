d7d9ec0c4d22c770583c45503a61eef25ee032f2
pmg.tadinmitev92 <pmg.tadinmitev92@gmail.com>
2024-10-15 11:45:34 +0300

    added data table added json resaponce from category controller

--- Ecomst/Controllers/CategoryController.cs
+++ Ecomst/Controllers/CategoryController.cs
@@ -86,54 +86,6 @@ namespace Ecomst.Controllers
             return View(viewModel);
         }
 
-        //public IActionResult Upsert(int? id)
-        //{
-        //    CategoryViewModel viewModel = new CategoryViewModel();
-        //    if(id == null)
-        //        return View(viewModel);
-
-        //    Category? category = _categoryService.GetCategoryById(id);
-        //    if (category == null)
-        //    {
-        //        TempData["error"] = "Category with id " + id + " not found!";
-        //        return RedirectToAction("Index");
-        //    }
-        //    viewModel.PopulateFromCategory(category);
-        //    return View(viewModel);
-        //}
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Upsert(CategoryViewModel viewModel)
-        //{
-        //    Category category = new Category();
-
-        //    if (viewModel.Id == null && ModelState.IsValid)
-        //    {
-        //        viewModel.PopulateCategory(category);
-        //        if (_categoryService.AddCategory(category))
-        //        {
-        //            TempData["success"] = "Category was created successfully!";
-        //            return RedirectToAction("Index");
-        //        }
-        //        TempData["error"] = "Unable to create category!";
-        //    }
-        //    else
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            viewModel.PopulateCategory(category);
-        //            if (_categoryService.UpdateCategory(category))
-        //            {
-        //                TempData["success"] = "Category was updated successfully!";
-        //                return RedirectToAction("Index");
-        //            }
-        //            TempData["error"] = "Unable to update category!";
-        //        }
-        //    }
-        //    return View(viewModel);
-        //}
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
@@ -147,6 +99,12 @@ namespace Ecomst.Controllers
                 TempData["error"] = "Unable to delete category";
             }
             return RedirectToAction("Index");
+        }
+
+        public IActionResult GetTableData()
+        {
+            List<Category> categoryList = _categoryService.GetCategoryList();
+            return Json(new { data = categoryList });
         }
     }
 }
---
+++ Ecomst/wwwroot/js/category/index.js
@@ -0,0 +1,32 @@
+﻿function loadDataTable() {
+    $("#category-table").DataTable({
+        ajax: { url: '/category/gettabledata' },
+        columns: [
+            { data: 'name', width: '45%' },
+            { data: 'displayOrder', width: '45%' },
+            {
+                data: 'id',
+                render: function (data) {
+                    return `<div class="w-100 btn-group" role="group">
+                        <a href="/category/update/id=${data}" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i> Edit
+                        </a>
+                        <form method="post" action="/category/delete" onsubmit="return confirm('Do you really want to delete category @obj.Name');">
+                            @Html.AntiForgeryToken()
+                            <input type="hidden" name="Id" value="${data}" />
+                            <button type="submit" class="btn btn-danger">
+                                <i class="bi-trash"></i>Delete
+                            </button>
+                        </form>
+                    </div>`;
+                },
+                width: '10%'
+            }
+        ]
+       }
+    );
+}
+
+$(document).ready(function () {
+    loadDataTable();
+});
