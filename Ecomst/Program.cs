bf30e899308cafb8730f006afce7bd0154bb02ed
pmg.tadinmitev92 <pmg.tadinmitev92@gmail.com>
2024-12-16 09:42:57 +0200

    Added Identity.

--- Ecomst/Program.cs
+++ Ecomst/Program.cs
@@ -5,6 +5,7 @@ using Ecomst.Services.IServices;
 using Ecomst.Services;
 using Microsoft.EntityFrameworkCore;
 using Ecomst.Seeds;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
